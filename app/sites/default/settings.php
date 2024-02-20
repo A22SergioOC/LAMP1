7749c77274229d8e643db1a6e01b3766ba63a374
Sergio Otero Castro <a22sergiooc@iessanclemente.net>
2024-02-20 12:37:52 +0100

    Second massive commit

---
+++ app/sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'db',
+  'username' => 'user',
+  'password' => 'user',
+  'prefix' => 'drupal_',
+  'host' => 'basedatos',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = '2ncqpeYj7XdqIBBmi4a36WD_RwPsLsX1ftuZ-GAhj9vZxbUjF0RX9HamUEtbON5SVGDdDXgdkw';
+$settings['config_sync_directory'] = 'sites/default/files/config_b6VqAF5rIniC3sVblQdsZAx170UsHd50CDt4XiOAYN3hjUqCR4AxRoiX2a0h6Nt_eGDpSVmY4w/sync';
